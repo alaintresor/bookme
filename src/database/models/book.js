18d4ea4d396ba404636dfd4572624f5180a93e02
Alain Tresor <tresoralain35@gmail.com>
2023-01-17 19:22:14 +0200

    add email functionality

--- src/database/models/book.js
+++ src/database/models/book.js
@@ -1,18 +1,18 @@
 'use strict';
 import { Model } from 'sequelize';
 export default (sequelize, DataTypes) => {
-    class Book extends Model {
+    class book extends Model {
         /**
          * Helper method for defining associations.
          * This method is not a part of Sequelize lifecycle.
          * The `models/index` file will call this method automatically.
          */
-        static associate({ users, Room, accomodation,events }) {
+        static associate({ users, Room, accomodation, events }) {
 
             this.belongsTo(users, { foreignKey: 'userId', as: 'user' });
             this.belongsTo(Room, { foreignKey: 'roomId', as: 'Room' });
             this.belongsTo(accomodation, { foreignKey: 'accomodationId', as: 'accomodation' });
-            this.belongsTo(events,{foreignKey:'eventId',as:'event'})
+            this.belongsTo(events, { foreignKey: 'eventId', as: 'event' })
         }
         toJSON() {
             return {
@@ -20,22 +20,32 @@ export default (sequelize, DataTypes) => {
             };
         }
     }
-    Book.init(
+    book.init(
         {
-            userId: DataTypes.INTEGER,
+            fname: DataTypes.STRING,
+            lname: DataTypes.STRING,
+            email: DataTypes.STRING,
+            title: DataTypes.STRING,
+            country: DataTypes.STRING,
+            phone: DataTypes.STRING,
+            WhoBookFor: DataTypes.STRING,
+            purpose: DataTypes.STRING,
+            promotionCode: DataTypes.STRING,
+            question: DataTypes.STRING,
+            arriveTime: DataTypes.STRING,
+            arriveDate: DataTypes.STRING,
             roomType: DataTypes.STRING,
             accomodationId: DataTypes.INTEGER,
             eventId: DataTypes.INTEGER,
             roomNumber: DataTypes.STRING,
             dayNumber: DataTypes.STRING,
-            date: DataTypes.STRING,
             status: DataTypes.STRING,
             comment: DataTypes.STRING
         },
         {
             sequelize,
-            modelName: 'Book',
+            modelName: 'book',
         },
     );
-    return Book;
+    return book;
 };
---
+++ src/utils/emailBooking.js
@@ -0,0 +1,67 @@
+import nodemailer from 'nodemailer';
+import ejs from 'ejs';
+import path from 'path';
+
+class EmailBooking {
+    constructor(data, hotel) {
+        this.to = data.email;
+        this.fname = data.fname;
+        this.hotel = hotel;
+        this.from = `Book Me <${process.env.EMAIL_FROM}>`;
+    }
+
+    newTransport() {
+        if (process.env.NODE_ENV === 'production') {
+            // Sendgrid
+            return nodemailer.createTransport({
+                service: 'SendGrid',
+                auth: {
+                    user: process.env.SENDGRID_USERNAME,
+                    pass: process.env.SENDGRID_PASSWORD,
+                },
+                tls: {
+                    rejectUnauthorized: false,
+                },
+                secure: false,
+            });
+        }
+
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+    }
+
+    // Send the actual email
+    async send(template, subject, title) {
+        // 1) Render HTML based on a ejs template
+        const html = await ejs.renderFile(
+            path.join(__dirname, `./../views/email/${template}.ejs`),
+            {
+                fname: this.fname,
+                hotel: this.hotel
+            },
+        );
+        // 2) Define email options
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject,
+            html,
+            text: html,
+        };
+        // 3) Create a transport and send email
+        await this.newTransport().sendMail(mailOptions);
+    }
+    async booking() {
+        if (process.env.NODE_ENV !== 'test') {
+            await this.send('booking', 'Booking confirming');
+        }
+    }
+}
+
+export default EmailBooking;
