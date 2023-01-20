d459df90d778dd5a5d87b3850847620a5397ca6a
Alain Tresor <tresoralain35@gmail.com>
2023-01-20 17:55:49 +0200

    fix issue

--- src/routers/api/users.js
+++ src/routers/api/users.js
@@ -7,12 +7,8 @@ import {
   protect,
   signup,
 } from '../../controllers/authentication';
-import {
-  getUserData,
-  updateUserProfile,
-  getAllUsers,
-} from '../../controllers/userController';
-import { updateRole } from '../../controllers/users';
+
+
 import isValidRole from '../../middlewares/isValidRole';
 import isAdmin from '../../middlewares/isAdmin';
 import multer from 'multer';
@@ -37,30 +33,16 @@ const userRouter = express.Router();
 userRouter.post('/signup', signup);
 userRouter.post('/logout', logout);
 userRouter.post('/login', login);
-userRouter.patch(
-  '/update',
-  protect,
-  uploads.single('image'),
-  updateUserProfile,
-);
-userRouter.get('/', getAllUsers);
-userRouter.get('/:id', protect, getUserData);
+
+
 userRouter.post('/signup', signup);
 userRouter.get('/notification/get', protect, getAllNotifications);
 userRouter.patch('/notification/:id', protect, readNotification);
 userRouter.put('/notification/read', protect, readAllNotification);
 userRouter.post('/logout', logout);
 userRouter.post('/login', login);
-userRouter.patch(
-  '/update',
-  protect,
-  uploads.single('image'),
-  updateUserProfile,
-);
-userRouter.get('/', getAllUsers);
+
 userRouter.post('/forgotpassword', forgotPassword);
 userRouter.patch('/resetpassword/:token', resetPassword);
-userRouter.get('/:id', protect, getUserData);
-userRouter.put('/roles', isAdmin, isValidRole, updateRole);
 
 export default userRouter;
